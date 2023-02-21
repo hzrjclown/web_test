d78b753a26458a34eb705e4a3f0ad68f6ee1b01d
HuJing <2358891978@qq.com>
2023-02-21 23:41:34 +0800

    第一次提交

---
+++ src/main/java/test1.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class test1 {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"error","error",JOptionPane.ERROR_MESSAGE);
+    }
+}
